7f935d3101770daa5d973b1613a6baaa29467ef4
FTC A <ftc18035@gmail.com>
2022-11-30 19:37:02 -0500

    Working drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDriveControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDriveControls.java
@@ -29,9 +29,9 @@ public class basicDriveControls extends LinearOpMode{
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
+            double y = gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = -gamepad1.right_stick_x;
 
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
