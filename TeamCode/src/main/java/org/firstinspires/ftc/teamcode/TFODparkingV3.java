a9850b6e6c9e44ba62de904c5bcdeadd20322012
FTC A <ftc18035@gmail.com>
2022-12-21 18:43:44 -0500

    TFOD parking but using 1 servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFODparkingV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFODparkingV3.java
@@ -56,7 +56,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@Autonomous(name = "TFOD_ParkingV1", group = "Concept")
+@Autonomous(name = "TFOD_Parking", group = "Concept")
 public class TFODparkingV3 extends LinearOpMode {
 
     /*
